dfbf3fb8320ef279c864a1eedc066a28e57325ac
Mary A Green <cyberangel9911@gmail.com>
2021-06-06 21:46:26 -0500

    column added

--- components/Readaway.js
+++ components/Readaway.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Readaway() {
-  return <p>hi world</p>;
+  return (
+    <Container fluid>
+      <Row>
+        <Col>1 of 1</Col>
+        <Col>2 of 2</Col>
+      </Row>
+    </Container>
+  );
 }
