f8dc1c50145f22720fc5c0ec69047deaad072701
Mary A Green <cyberangel9911@gmail.com>
2021-07-13 18:16:44 -0500

    form
    adjusting form

--- components/ValidateLogin.js
+++ components/ValidateLogin.js
@@ -1,26 +1,55 @@
-export default () => {
+import { CheckCircle } from 'react-bootstrap-icons';
+import { Form, Button } from 'react-bootstrap';
+//work on only getting react bootstrap items into project note regular bootstrap, rework form and style.
+const ValidateLogin = () => {
   return (
-    <form className="login_form">
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    /*<form className="login_form">
       <h5 className="login">Log in</h5>
       <div className="mb-3">
-        <label for="exampleInputUsername" className="form-label">
+        <label htmlFor="exampleInputUsername" className="form-label">
           UserName
         </label>
         <input
           type="text"
-          className="form-control"
+          className="form-control success error"
           aria-describedby="UsernameHelp"
+          placeholder="Username"
         />
+        <CheckCircle />
+        <i className="bi bi-exclamation-circle-fill"></i>
       </div>
       <div className="mb-3 mb-0">
-        <label for="exampleInputPassword1" className="form-label">
+        <label htmlFor="exampleInputPassword1" className="form-label">
           Password
         </label>
         <input
           type="password"
-          className="form-control"
+          className="form-control success error"
           id="exampleInputPassword1"
+          placeholder="Password"
         />
+        <i className="bi bi-check-circle"></i>
+        <i className="bi bi-exclamation-circle-fill"></i>
       </div>
       <div>
         <a href="#" className="link-primary">
@@ -30,6 +59,7 @@ export default () => {
       <button type="submit" className="btn btn-primary mt-3">
         Submit
       </button>
-    </form>
+    </form>*/
   );
 };
+export default ValidateLogin;
