52747331a2edf8c0fc82033a60372f33539133bd
Mary A Green <cyberangel9911@gmail.com>
2021-07-29 22:28:45 -0500

    Yup and React Hooks
    added Yup and react hooks

--- components/ValidateLogin.js
+++ components/ValidateLogin.js
@@ -1,11 +1,31 @@
 import { Form, Button } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
 const ValidateLogin = () => {
-  //validate the form next
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(6).max(12).required()
+  });
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const formSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <Form className="login_form">
+    <Form onSubmit={handleSubmit} className="login_form">
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          ref={register}
+        />
+        <p>{errors.email?.message}</p>
+
         <Form.Text className="form-label">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -13,7 +33,13 @@ const ValidateLogin = () => {
 
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          name="password"
+          placeholder="Password"
+          ref={register}
+        />
+        <p>{errors.password?.message}</p>
       </Form.Group>
       <Form.Group controlId="formBasicCheckbox"></Form.Group>
       <Button variant="primary" type="submit">
