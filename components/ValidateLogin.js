c0c45e0da3e615695460cede7f067f6d6af7f399
Mary A Green <cyberangel9911@gmail.com>
2021-08-05 17:02:01 -0500

    Formik
    added Formik and Yup

--- components/ValidateLogin.js
+++ components/ValidateLogin.js
@@ -1,21 +1,64 @@
-import { Form, Button } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { Form, Row, Col, Container, Button } from 'react-bootstrap';
 import * as yup from 'yup';
+import { Formik } from 'formik';
 
 const ValidateLogin = () => {
+  //install formik and redo form and read it on react boot strap js
   const schema = yup.object().shape({
     email: yup.string().email().required(),
     password: yup.string().min(6).max(12).required()
   });
-  const { register, handleSubmit, errors } = useForm({
-    resolver: yupResolver(schema)
-  });
-  const formSubmit = (data) => {
-    console.log(data);
-  };
+
   return (
-    <Form onSubmit={handleSubmit} className="login_form">
+    <Formik
+      validateSchema={schema}
+      initialValues={{
+        email: '',
+        password: ''
+      }}
+    >
+      {({ handleSubmit, handleChange, values, touched, errors }) => (
+        <Form noValidate onSubmit={handleSubmit}>
+          <Container>
+            <Row>
+              <Col md={12}>
+                <Form.Group controlId="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    isValid={touched.email && !errors.email}
+                    placeholder="Email"
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col md={12}>
+                <Form.Group controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    value={values.password}
+                    onChange={handleChange}
+                    isValid={touched.password && !errors.password}
+                    placeholder="Password"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+          </Container>
+        </Form>
+      )}
+    </Formik>
+  );
+
+  /*<Form onSubmit={handleSubmit} className="login_form">
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email</Form.Label>
         <Form.Control
@@ -24,7 +67,6 @@ const ValidateLogin = () => {
           placeholder="Enter email"
           ref={register}
         />
-        <p>{errors.email?.message}</p>
 
         <Form.Text className="form-label">
           We'll never share your email with anyone else.
@@ -39,13 +81,11 @@ const ValidateLogin = () => {
           placeholder="Password"
           ref={register}
         />
-        <p>{errors.password?.message}</p>
       </Form.Group>
       <Form.Group controlId="formBasicCheckbox"></Form.Group>
       <Button variant="primary" type="submit">
         Submit
       </Button>
-    </Form>
-  );
+  </Form>*/
 };
 export default ValidateLogin;
