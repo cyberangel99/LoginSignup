7c59e23f37e1e82966a934a7e96f7dd0fe401aed
Mary A Green <cyberangel9911@gmail.com>
2021-08-16 21:13:52 -0500

    signout
    added a signout button

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import firebase from '../utils/firebase';
 import { useRouter } from 'next/router';
-
+//check to see of token is removed when signing out
 export default function Dashboard() {
+  //Signout
+  const router = useRouter();
+  const signout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        router.push('/');
+      });
+  };
   return (
     <section className="dashboard">
-      <h1 className="loginTitle">You successfully logged into Readaway!</h1>
+      <h1 className="loginTitle">You successfully signed into Readaway!</h1>
+      <Button className="signout" onClick={() => signout()}>
+        signout
+      </Button>
     </section>
   );
 }
